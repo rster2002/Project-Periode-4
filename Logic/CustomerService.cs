b67a8a6da27b37140e1c34ea930a6ec6a770e132
NiekRoelofs <54812507+NiekRoelofs@users.noreply.github.com>
2020-05-11 16:21:28 +0200

    DAO+Service van Table en Customer

---
+++ Logic/CustomerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DAL;
+
+namespace Logic {
+    public class CustomerService {
+
+        private CustomerDAO customerDAO = new CustomerDAO();
+
+        public List<Customer> GetAllCustomers() {
+            List<Customer> customers = customerDAO.GetAll();
+            return customers;
+        }
+        public Customer GetBySurname(string surname) {
+            Customer customer = customerDAO.GetBySurname(surname);
+            return customer;
+        }
+    }
+}
---
+++ Logic/TableService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using DAL;
+
+namespace Logic {
+    public class TableService {
+
+        private tableDAO tableDAO = new tableDAO();
+
+        public List<Table> GetAllTables() {
+            List<Table> tables = tableDAO.GetAll();
+            return tables;
+        }
+        public Table GetTableById(int tableId) {
+            Table table = tableDAO.GetById(tableId);
+            return table;
+        }
+        public List<Table> GetTablesByStaff(int staffId) {
+            List<Table> tables = tableDAO.GetByStaff(staffId);
+            return tables;
+        }
+    }
+}
