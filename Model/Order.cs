6d289d0c1a3eac5523429e760d757278be23a2cf
rster20002 <rster2002dev@gmail.com>
2020-05-18 15:52:40 +0200

    Added PlacedAt property to Order Model

--- Model/Order.cs
+++ Model/Order.cs
@@ -1,7 +1,10 @@
-﻿namespace Model {
+﻿using System;
+
+namespace Model {
     public class Order {
         public int Id { get; set; }
         public Table Table { get; set; }
         public Staff PlacedBy { get; set; }
+        public DateTime PlacedAt { get; set; }
     }
 }
