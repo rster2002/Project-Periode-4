9a346bfa2e737588a7e947c1f39f5cd691ab57e2
rster20002 <rster2002dev@gmail.com>
2020-05-21 15:50:15 +0200

    Fixed multiple definitions for 'PlacedAt'

--- Model/Order.cs
+++ Model/Order.cs
@@ -5,7 +5,6 @@ namespace Model {
     public class Order {
         public int Id { get; set; }
         public Staff PlacedBy { get; set; }
-        public DateTime PlacedAt { get; set; }
         public List<MenuItem> MenuItems { get; set; }
         public DateTime PlacedAt { get; set; }
         public string Tag { get; set; }
