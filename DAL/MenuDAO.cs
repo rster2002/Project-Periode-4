7ddbfa829c8e5b9b85e4304fbbf21b930eabe27c
Ryan-Erfmann <57288223+Ryan-Erfmann@users.noreply.github.com>
2020-05-22 10:25:40 +0200

    update MenuDAO en MenuItemDAO

--- DAL/MenuDAO.cs
+++ DAL/MenuDAO.cs
@@ -31,5 +31,31 @@ namespace DAL {
                 EndTime = (TimeSpan) record["MenuAvailableEndDateTime"]
             };
         }
+        public override List<Menu> ProcessRecords(List<Record> records) {
+            Dictionary<int, Menu> menuMap = new Dictionary<int, Menu>();
+            MenuItemDAO menuItemDAO = new MenuItemDAO();
+
+            foreach (Record record in records) {
+                int menuId = (int) record["MenuId"];
+
+                // Check whether or not reservation has been processed yet
+                if (!menuMap.ContainsKey(menuId)) {
+                    Menu menu = ProcessRecord(record);
+
+                    // Because 'ProcessRecords' is public, we can ask other DAO's to process certain records for us
+                    menu.Items = menuItemDAO.ProcessRecords(
+                        // We only want to process the records that apply to the current rmenu, so we filter out any that don't
+                        // match the current menu id
+                        records
+                            .Where(r => (int) r["MenuId"] == menuId)
+                            .ToList()
+                    );
+
+                    menuMap[menuId] = menu;
+                }
+            }
+
+            return menuMap.Values.ToList();
+        }
     }
 }
