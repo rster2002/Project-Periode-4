aaacca10bdea7e7736fb2fbfc36f898306a84cb6
rster20002 <rster2002dev@gmail.com>
2020-05-26 21:59:57 +0200

    Fixed 'UsesSession not found'

---
+++ Model/UserSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Model {
+    public class UserSession {
+        private static UserSession instance;
+        public Staff LoggedInStaff { get; private set; }
+
+        private UserSession() {
+            LoggedInStaff = new Staff() {
+                Role = "chef"
+            };
+        }
+
+        public static UserSession GetInstance() {
+            if (instance == null) instance = new UserSession();
+            return instance;
+        }
+
+        public bool Login() {
+            return true;
+        }
+    }
+}
---
+++ UI/ErrorView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI {
+    public partial class ErrorView: Form {
+        public ErrorView(string errorMessage) {
+            InitializeComponent();
+
+            lblErrorMessage.Text = errorMessage;
+        }
+
+        private void CloseDialogButton_OnClick(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
