dd9efc4ddee427560cb21743b87f68face46c2dd
rster20002 <rster2002dev@gmail.com>
2020-05-26 17:26:04 +0200

    Continued working on UI foundation

---
+++ UI/DesktopView.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace UI {
+    partial class DesktopView {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.testViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.barToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(181)))), ((int)(((byte)(181)))));
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 59);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1904, 982);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.menuStrip.Font = new System.Drawing.Font("Segoe UI", 28F);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testViewToolStripMenuItem,
+            this.barToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1904, 59);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // testViewToolStripMenuItem
+            // 
+            this.testViewToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.testViewToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.testViewToolStripMenuItem.Name = "testViewToolStripMenuItem";
+            this.testViewToolStripMenuItem.Size = new System.Drawing.Size(158, 55);
+            this.testViewToolStripMenuItem.Tag = "chef";
+            this.testViewToolStripMenuItem.Text = "Keuken";
+            this.testViewToolStripMenuItem.Click += new System.EventHandler(this.keukenToolStripMenuItem_Click);
+            // 
+            // barToolStripMenuItem
+            // 
+            this.barToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.barToolStripMenuItem.Name = "barToolStripMenuItem";
+            this.barToolStripMenuItem.Size = new System.Drawing.Size(88, 55);
+            this.barToolStripMenuItem.Tag = "bartender";
+            this.barToolStripMenuItem.Text = "Bar";
+            // 
+            // DesktopView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.menuStrip);
+            this.Name = "DesktopView";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem testViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem barToolStripMenuItem;
+    }
+}
+
---
+++ UI/DesktopView.cs
@@ -0,0 +1,61 @@
+﻿using Model;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI {
+    public partial class DesktopView:Form {
+        public static DesktopView instance;
+        public UserControl loadedView;
+        public Staff loggedIn = new Staff() {
+            Role = "chef"
+        };
+
+        public static DesktopView GetInstance() {
+            if (instance == null) instance = new DesktopView();
+
+            return instance;
+        }
+
+        private DesktopView() {
+            InitializeComponent();
+        }
+
+        private void ShowTabs() {
+            for (int i = 0; i < menuStrip.Items.Count; i++) {
+                ToolStripItem item = menuStrip.Items[i];
+
+                item.Visible = ((string) item.Tag).Contains(loggedIn.Role);
+
+                // Maybe
+                //if (loadedView != null && item.Name.ToLower().Contains(loadedView.Name.ToLower())) {
+                //    item.BackColor = Color.FromArgb(255, 255, 255);
+                //} else {
+                //    item.BackColor = Color.FromArgb(33, 33, 33);
+                //}
+            }
+        }
+
+        public void LoadView(UserControl userControl) {
+            loadedView = userControl;
+
+            ShowTabs();
+            mainPanel.Controls.Clear();
+            userControl.Dock = DockStyle.Fill;
+
+            mainPanel.Controls.Add(userControl);
+        }
+
+        private void keukenToolStripMenuItem_Click(object sender, EventArgs e) {
+            LoadView(new TestView());
+        }
+    }
+}
