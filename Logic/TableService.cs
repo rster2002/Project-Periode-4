4dd10d15f2e7dc9c6bc506012d1991aefff64bcc
rster20002 <rster2002dev@gmail.com>
2020-06-08 10:41:14 +0200

    Removed try catch from services

--- Logic/TableService.cs
+++ Logic/TableService.cs
@@ -11,61 +11,8 @@ namespace Model {
 
         private TableDAO tableDAO = new TableDAO();
 
-        public List<Table> GetAllTables() {
-            try {
-                List<Table> tables = tableDAO.GetAll();
-                return tables;
-            }
-            catch {
-                List<Table> tables = new List<Table>();
-                Table table = new Table {
-                    Number = 200,
-                    NumberOfSeats = 10,
-                    ServedBy = new Staff {
-                        Id = 200,
-                        Name = "StaffTester"
-                    }
-                };
-                tables.Add(table);
-                return tables;
-            }
-        }
-        public Table GetTableById(int tableId) {
-            try {
-                Table table = tableDAO.GetById(tableId);
-                return table;
-            }
-            catch {
-                Table table = new Table {
-                    Number = 200,
-                    NumberOfSeats = 10,
-                    ServedBy = new Staff {
-                        Id = 200,
-                        Name = "StaffTester"
-                    }
-                };
-                return table;
-            }
-        }
-        public List<Table> GetTablesByStaff(int staffId) {
-            try {
-                List<Table> tables = tableDAO.GetByStaff(staffId);
-                return tables;
-            }
-            catch {
-                List<Table> tables = new List<Table>();
-                Table table = new Table {
-                    Number = 200,
-                    NumberOfSeats = 10,
-                    ServedBy = new Staff {
-                        Id = 200,
-                        Name = "StaffTester"
-                    }
-                };
-                tables.Add(table);
-                return tables;
-            }
-            
-        }
+        public List<Table> GetAllTables() => tableDAO.GetAll();
+        public Table GetTableById(int tableId) => tableDAO.GetById(tableId);
+        public List<Table> GetTablesByStaff(int staffId) => tableDAO.GetByStaff(staffId);
     }
 }
