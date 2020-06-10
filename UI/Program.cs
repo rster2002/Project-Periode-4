91560a2bf0d67ed2a91bebb19cd8e163435b951e
Unknown <JunkmailBelith@gmail.com>
2020-06-10 21:15:41 +0200

    Edits after merge conflicts

--- UI/Program.cs
+++ UI/Program.cs
@@ -14,6 +14,8 @@ namespace UI {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            Application.Run(new ViewPicker());
+
             //try {
             //    Application.Run(new ViewPicker());
             //} catch(Exception error) {
